14dd49947b8e5d4d9c91a84d9e7e88b3c51a1f4b
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-03-22 23:54:22 +0500

    Tutorial 2 code

---
+++ index.js
@@ -0,0 +1,40 @@
+// 1. Comments
+
+// 2. Variables
+// vary - change 
+// 1. Declaration
+// 2. Initialization
+
+var box
+box = "cake"
+
+var num = 24
+
+var cakeIsPlaced = true
+
+// var 1abc; wrong
+// var abc1; right
+
+// var a bc; wrong
+// var abc; right
+
+// var a'b; wrong
+// var a_b; right
+
+//case sensitive
+var a=5;
+var A=4;
+
+var cake;
+var Cake;
+
+// syntax
+// keyword/reserved word
+// identifiers
+// assignment operator =
+// ; statement terminator
+// Data types
+// string
+// numbers
+// boolean
+
