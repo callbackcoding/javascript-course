faceddd0bd97e443c7e1b195d0ecd3c816d62739
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-03-25 22:24:45 +0500

    Tutorial 3 code (Inputs & Outputs)

---
+++ tut3.js
@@ -0,0 +1,35 @@
+// // ONE INPUT WAY
+// // 1. prompt
+
+// //THREE OUTPUT WAYS
+// // 1. alert
+// // 2. console
+// // 3. document.write
+
+//input
+
+var myName = prompt("Enter your name", "My name is"); //string
+// var myNum = +prompt("Enter your name", "My name is"); //numbers
+
+//output
+
+// alert("Hello world!"); 
+
+// alert(myName);
+
+// alert(55)
+
+// alert(true)
+
+// console.log("Hello world!");
+
+// console.log(myName);
+// console.log(55);
+// console.log(true);
+
+
+// document.write("Hello world! <br> ");
+// document.write(55 + "<br>");
+// document.write(true);
+document.write(myName);
+
