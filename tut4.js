90e6c3ad9dfb264113044334584bdd1313291071
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-03-28 16:03:10 +0500

    Tutorial 4 code (Operators & Concatenation)

---
+++ tut4.js
@@ -0,0 +1,37 @@
+// 1. Mathematical Operators (+ , - , * , / , %)
+// 2. Increment and Decrement Operator ( ++ , -- )
+// 3. Concatenation (+)
+
+// var sum = 2 + 3;
+// var a = 6;
+// var b = 3;
+// var c = a + b;
+// console.log(sum);
+// console.log("value of c: ", c);
+// console.log("value of a+b: ", a + b);
+// console.log("value of a-b: ", a - b);
+// console.log("value of b-a: ", b - a);
+// console.log("value of b*a: ", b * a);
+// console.log("value of b/a: ", b / a);
+// console.log("value of a/b: ", a / b);
+// console.log("value of a%b: ", a % b);
+// console.log("value of 5%2: ", 5 % 2);
+
+// var num = 5;
+// // var x = ++num; //prefix
+// var x = num++; //postfix
+// console.log("x: ", x);
+// console.log("num: ", num);
+
+// var num2 = 4;
+// var y = --num2; // prefix
+// var y = num2--; // postfix
+// console.log("y: ", y);
+// console.log("num2: ", num2);
+
+//Concatenation (+)
+
+console.log("My" + " name is");
+var name = "Callback Coding";
+console.log("Welcome to " + name + " 2023");
+console.log("Num: " + 3);
