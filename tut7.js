15024b1403461306746c9de4f709b0c5214c0bb5
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-04-01 00:41:00 +0500

    Tutorial # 07 (For Loop)

---
+++ tut7.js
@@ -0,0 +1,69 @@
+// ITERATIVE STATEMENTS (LOOPS)
+
+// -- use for repetition
+
+// 1. FOR LOOP
+// 2. WHILE LOOP
+// 3. DO WHILE LOOP
+
+// for loop
+
+// for(initialization;condition;increment){
+//     body
+// }
+
+// for (var i = 0; i < 20; i++) {
+//   console.log("Welcome to callback coding");
+//   document.write(i + 1, " Welcome to callback coding <br>");
+// }
+
+// 2 x 1 = 2
+// 2 x 2 = 4
+// 2 x 3 = 6
+// 2 x 4 = 8
+// 2 x 5 = 10
+// 2 x 6 = 12
+// 2 x 7 = 14
+// 2 x 8 = 16
+// 2 x 9 = 18
+// 2 x 10 = 20
+
+// console.log("2 x 1 = 2") // wrong approach
+
+//loop
+
+// for (var i = 1; i <= 10; i++) {
+//   console.log(2 + " x " + i + " = " + 2 * i);
+// }
+// console.log(i);
+// i =1 , true , body execute, inc , cond , body ex.
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+
+// rows = 5
+
+for(var i=1;i<=5;i++){
+    for(var j=0;j<i;j++){
+       document.write("* ")
+    }
+    document.write("<br>")
+}
+
+// i=1, j=0 //1st iteration
+// *
+
+// i=2, j=0
+// i=2, j=1
+// * *
+
+
+// i=3, j=0
+// i=3, j=1
+// i=3, j=2
+
+// * * *
