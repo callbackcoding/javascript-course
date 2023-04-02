bafd1a1c7f111b81933e5e85ca7c472f9388d061
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-04-02 23:44:29 +0500

    Tutorial 9 ( While and Do while loop )

---
+++ tut9.js
@@ -0,0 +1,31 @@
+// While Loop (no. of iterations are unknown)
+// Do While Loop
+
+// while(condition){
+//     // body
+// }
+
+// //for loop
+// for (var i = 0; i < 5; i++) {
+//   console.log("Value of i is: ", i);
+// }
+
+// //while loop
+// var j = 0;
+// while (j < 5) {
+//   console.log("Value of j is: ", j);
+//   j++;
+// }
+
+// var num = -1;
+// while (num != 5) {
+//   num = +prompt("Enter your number");
+// }
+
+//do while loop
+
+// var num = 0;
+// do {
+//   num = +prompt("Enter your number");
+// } while (num != 0);
+
