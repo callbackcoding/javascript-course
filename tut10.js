e0e2e7dc655182d5c63f9ee31c12d0e9513cd4dd
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-04-12 00:29:33 +0500

    Tutorial # 10 (Functions)

---
+++ tut10.js
@@ -0,0 +1,52 @@
+// Functions in JS
+
+// var totalMarks = +prompt("Enter total marks");
+// var obtainedMarks = +prompt("Enter obtained marks");
+// var a = obtainedMarks / totalMarks;
+// var per = a * 100;
+// console.log("The percentage is: ", per);
+
+//input
+//division
+//multiply
+//output
+
+//1. declare or define
+//2. call
+
+// function perCal() {
+//   var totalMarks = +prompt("Enter total marks");
+//   var obtainedMarks = +prompt("Enter obtained marks");
+//   var a = obtainedMarks / totalMarks;
+//   var per = a * 100;
+//   console.log("The percentage is: ", per);
+// }
+
+// perCal()
+// perCal()
+// perCal()
+// perCal()
+// perCal()
+// perCal()
+// perCal();
+
+// parameters
+// arguments
+// return
+
+//parameter
+function birthdayParty(Abbu, Bhai) {
+  console.log("Yahan party ho rhi hai !");
+  console.log("khana lg gya hai or khane me ", Abbu, "hai");
+  console.log("Or meethe me ", Bhai, "hai");
+  return 15000;
+}
+
+var khana = "Biryani";
+var meetha = "Custard";
+
+var owner = birthdayParty(khana, meetha); //arguments
+
+console.log("owner: ", owner);
+
+// console.log(birthdayParty(khana, meetha));
