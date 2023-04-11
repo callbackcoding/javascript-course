0ff0d20e39bd473f8e5c4b49b105b22ecf91ea3d
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-04-12 00:31:54 +0500

    Tutorial # 13 (Events )

---
+++ tut13.js
@@ -0,0 +1,21 @@
+// EVENTS
+
+// 1. onclick (When mouse click on an element)
+// 2. ondblclick (The user double-clicks on an element)
+// 3. onchange (When the user modifies or changes the value of a form element)
+
+// 4. onfocus (When the user focuses on an element)
+// 5. onblur (When the focus is away from a form element)
+
+// 6. onmouseover (When the cursor of the mouse comes over the element)
+// 7. onmouseout (When the cursor of the mouse leaves an element)
+
+// 8. onkeydown (When the user presses the key)
+// 9. onkeypress (When the user presses the key)
+
+//10. onresize (When the visitor resizes the window of the browser)
+
+function greeting() {
+  alert("Welcome to Callback Coding");
+  console.log("hello")
+}
