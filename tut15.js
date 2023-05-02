e04212e423224c50947d22b160089499ca7facb5
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-05-02 20:21:09 +0500

    Tut # 15 (Applying CSS by JavaScript)

---
+++ tut15.js
@@ -0,0 +1,14 @@
+var h1 = document.getElementById("h1");
+var img = document.getElementById("myImg");
+// console.log(h1);
+  console.log(img);
+
+function changeColor() {
+  h1.style.color = "red";
+}
+
+function hide() {
+  img.className = "hide";
+  console.log(img)
+  // img.style.display = "none";
+}
