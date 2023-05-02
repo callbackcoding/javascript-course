e577208514e535c60111e911a8d0c475e600c92d
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-05-02 20:22:06 +0500

    Tut # 16 (DOM, Junk Artifacts,Parent, Children and Siblings)

---
+++ tut16.js
@@ -0,0 +1,17 @@
+var div = document.getElementById("div");
+var myList = document.getElementById("myList");
+
+// console.log(div.childNodes);
+// console.log(div.childNodes[1].childNodes[1]);
+// console.log(div.firstChild.nextSibling);
+// console.log(div.lastChild.previousSibling);
+
+// console.log(myList.parentNode.parentNode);
+// console.log(myList.parentNode.nextSibling.nextSibling);
+
+
+console.log(div.children)
+console.log(div.firstElementChild);
+console.log(div.lastElementChild);
+console.log(div.firstElementChild.nextElementSibling);
+console.log(myList.parentElement.parentElement)
