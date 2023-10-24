9e79892a703de75970648219007afd0fd43d1836
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-10-24 18:56:22 +0500

    Variable Declaration and Hoisting

---
+++ tut18.js
@@ -0,0 +1,41 @@
+//Variable Declaration and Hoisting
+
+//let , const , var
+
+// var a = 5; // functional scope
+
+// let b = 5; // block scope
+// const c = 5;
+
+// if (true) {
+//   console.log("Hello");
+//   // let sum = 100;
+//   // const sum = 100;
+//   var product = 400;
+// }
+
+// console.log(product);
+// console.log(sum);
+
+// function add() {
+//   console.log("Add");
+//   var a = 9;
+// }
+
+// console.log(a);
+// add();
+
+// const n = 100;
+// n=19
+// console.log(n);
+
+// console.log(a)
+
+// var a;
+
+
+add();
+
+function add() {
+  console.log(3 + 9);
+}
