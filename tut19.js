57427289cedde3496ce0d0cc7b9ac4094f412b0c
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-11-11 18:27:32 +0500

    Promises and Async Await ( Tutorial # 19 )

---
+++ tut19.js
@@ -0,0 +1,25 @@
+// Cookies
+// Local Storage
+// Session Storage
+
+// document.cookie = "username=callback";
+// document.cookie = "id=A34345B; path=/index.html";
+
+// document.cookie = "id=434683482; expires=Wed, 18 Dec 2013 12:00:00 UTC;";
+
+// console.log(document.cookie);
+
+// localStorage.setItem("username", "callback");
+// localStorage.setItem("id", "686788");
+
+// var a = localStorage.getItem("id");
+// localStorage.removeItem("id")
+// console.log(a)
+
+
+// sessionStorage.setItem("username", "callback");
+// sessionStorage.setItem("id", "686788");
+
+// var a = sessionStorage.getItem("id");
+// sessionStorage.removeItem("id");
+// console.log(a);
