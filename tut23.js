6609ae599e6ffa5d6da15ef3d89050ac1245abd4
callbackcoding <112818291+callbackcoding@users.noreply.github.com>
2023-11-21 16:24:02 +0500

    Tut # 23 (Map, Find, Filter, ForEach, Some)

---
+++ tut23.js
@@ -0,0 +1,35 @@
+//map
+//forEach
+
+//filter
+//find
+//some
+
+var arr = [1,2,3,4,5,"A"]
+
+//map 
+
+// arr.map((item,index)=>{
+//     console.log(index + " " + item)
+// })
+
+//forEach
+
+// arr.forEach((item,index)=>{
+//     console.log(index + " " + item)
+// })
+
+//filter (returns array)
+
+// var x = arr.filter((a)=>a>2)
+// console.log(x)
+
+//find
+
+// var x = arr.find((a)=>a>2)
+// console.log(x)
+
+//some (returns boolean)
+
+// var x = arr.some((a)=>a>10)
+// console.log(x)
